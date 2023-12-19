1a5ea3ad7e9927aefc10459a0c0671ad255c8c7f
Jon Matthis <jonmatthis@gmail.com>
2023-12-19 13:39:56 -0600

    restart wtih nestjs

---
+++ src/app.controller.ts
@@ -0,0 +1,18 @@
+import { Controller, Get } from '@nestjs/common';
+import { AppService } from './app.service';
+import { HelloTwoService } from "./HelloTwoService";
+
+@Controller()
+export class AppController {
+  private appService: AppService;
+  private helloTwo: HelloTwoService;
+  constructor(appService: AppService, helloTwo: HelloTwoService) {
+    this.appService = appService;
+    this.helloTwo = helloTwo;
+  }
+
+  @Get('/hello')
+  getHello(): string {
+    return this.helloTwo.getOtherHello();
+  }
+}
