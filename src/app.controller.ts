f14aa1ba450e20f9f38cca0c5ff4b36bb0326610
Jon Matthis <jonmatthis@gmail.com>
2023-12-19 18:19:12 -0600

    clean up imports

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,6 +1,6 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
-import { HelloTwoService } from "./HelloTwoService";
+import { HelloTwoService } from './HelloTwoService';
 
 @Controller()
 export class AppController {
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,4 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { HelloTwoService } from './HelloTwoService';
 import { MyDiscordModule } from './discord/myDiscord.module';
 
 @Module({
