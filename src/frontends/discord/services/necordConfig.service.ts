dc955f93d3abef8cbbc0f5bd07137d5122a9df26
Endurance Idehen <jeidehen@gmail.com>
2023-12-22 10:44:06 -0600

    SKELELYBOT DEV IS (GONNA) BE ALIVE

--- src/frontends/discord/services/necordConfig.service.ts
+++ src/frontends/discord/services/necordConfig.service.ts
@@ -3,13 +3,24 @@ import { NecordModuleOptions } from 'necord';
 import { IntentsBitField } from 'discord.js';
 import { SecretsManagerService } from '../../../gcp/secretsManager.service';
 
+const DISCORD_API_TOKEN =
+  'projects/588063171007/secrets/DISCORD_API_TOKEN/versions/latest';
+
+const DISCORD_API_DEV_TOKEN =
+  'projects/588063171007/secrets/DISCORD_API_DEV_TOKEN/versions/latest';
+
 @Injectable()
 export class NecordConfigService {
   constructor(private readonly sms: SecretsManagerService) {}
 
   async createNecordOptions(): Promise<NecordModuleOptions> {
+    const secretName =
+      process.env.NODE_ENV === 'production'
+        ? DISCORD_API_TOKEN
+        : DISCORD_API_DEV_TOKEN;
+
     const [secret] = await this.sms.getManager().accessSecretVersion({
-      name: 'projects/588063171007/secrets/DISCORD_API_TOKEN/versions/latest',
+      name: secretName,
     });
     const token = secret.payload.data.toString();
     return {
