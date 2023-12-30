bcf018ad6e2b6c4732ce1dc5971a526d1eb37596
jonmatthis <jonmatthis@gmail.com>
2023-12-30 14:48:38 -0500

    delete autogenerated thread module

--- src/src/interfaces/chat/discord/thread/thread.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { ThreadService } from './thread.service';
-import { ThreadController } from './thread.controller';
-
-@Module({
-  controllers: [ThreadController],
-  providers: [ThreadService],
-})
-export class ThreadModule {}
