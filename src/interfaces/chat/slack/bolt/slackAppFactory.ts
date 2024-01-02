fe1855b58ead98c97993b05c081bd30ccda5e4db
Endurance Idehen <jeidehen@gmail.com>
2024-01-02 00:04:21 -0600

    add logging

--- src/interfaces/chat/slack/bolt/slackAppFactory.ts
+++ src/interfaces/chat/slack/bolt/slackAppFactory.ts
@@ -1,11 +1,16 @@
 import { App } from '@slack/bolt';
 import { SlackConfigService } from '../config/slackConfig.service';
+import { SlackLoggerProxy } from '../logging/slack-logger-proxy.service';
 
 export const slackServiceFactory = {
   provide: App,
-  inject: [SlackConfigService],
-  useFactory: async (scs: SlackConfigService) => {
+  inject: [SlackConfigService, SlackLoggerProxy],
+  useFactory: async (scs: SlackConfigService, logger: SlackLoggerProxy) => {
     const opts = await scs.createSlackOptions();
-    return new App(opts);
+    return new App({
+      ...opts,
+      logger,
+      // logLevel: LogLevel.DEBUG,
+    });
   },
 };
--- src/interfaces/chat/slack/bolt/slackBolt.module.ts
+++ src/interfaces/chat/slack/bolt/slackBolt.module.ts
@@ -3,10 +3,11 @@ import { Module, OnModuleInit } from '@nestjs/common';
 import { App } from '@slack/bolt';
 import { SlackConfigService } from '../config/slackConfig.service';
 import { GcpModule } from '../../../../shared/gcp/gcp.module';
+import { SlackLoggerProxy } from '../logging/slack-logger-proxy.service';
 
 @Module({
   imports: [GcpModule],
-  providers: [slackServiceFactory, SlackConfigService],
+  providers: [slackServiceFactory, SlackConfigService, SlackLoggerProxy],
   exports: [slackServiceFactory],
 })
 export class SlackBoltModule implements OnModuleInit {
