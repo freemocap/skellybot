8686b1bf165ed7d96a0d6609d76bbeef3a1c4625
jonmatthis <jonmatthis@gmail.com>
2024-01-05 05:57:32 -0500

    getting there

--- src/shared/database2/cats/cats.controller.ts
+++ src/shared/database2/cats/cats.controller.ts
@@ -1,4 +1,12 @@
-import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
 import { CatsService } from './cats.service';
 import { CreateCatDto } from './dto/create-cat.dto';
 import { Cat } from './schemas/cat.schema';
@@ -35,6 +43,25 @@ export class CatsController {
   })
   async findOne(@Param('id') id: string): Promise<Cat> {
     return this.catsService.findOne(id);
+  }
+  @Post(':id')
+  @ApiOperation({ summary: 'Update cat' })
+  @ApiResponse({ status: 200, description: 'The updated record', type: Cat })
+  async update(
+    @Param('id') id: string,
+    @Body() updateCatDto: CreateCatDto,
+  ): Promise<Cat> {
+    return this.catsService.update(id, updateCatDto);
+  }
+
+  @Post('upsert')
+  @ApiOperation({ summary: 'Upsert cat' })
+  @ApiResponse({ status: 200, description: 'The upserted record', type: Cat })
+  async upsert(
+    @Body() filter: object,
+    @Body() updateCatDto: CreateCatDto,
+  ): Promise<Cat> {
+    return this.catsService.upsert(filter, updateCatDto);
   }
 
   @Delete(':id')
--- src/shared/database2/cats/cats.service.ts
+++ src/shared/database2/cats/cats.service.ts
@@ -1,16 +1,15 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
-import { CreateCatDto } from './dto/create-cat.dto';
-import { Cat } from './schemas/cat.schema';
+import { Cat } from './cat.schema';
+import { CreateCatDto } from './create-cat.dto';
 
 @Injectable()
 export class CatsService {
   constructor(@InjectModel(Cat.name) private readonly catModel: Model<Cat>) {}
 
   async create(createCatDto: CreateCatDto): Promise<Cat> {
-    const createdCat = await this.catModel.create(createCatDto);
-    return createdCat;
+    return await this.catModel.create(createCatDto);
   }
 
   async findAll(): Promise<Cat[]> {
@@ -19,6 +18,18 @@ export class CatsService {
 
   async findOne(id: string): Promise<Cat> {
     return this.catModel.findOne({ _id: id }).exec();
+  }
+
+  async update(id: string, updateCatDto: CreateCatDto): Promise<Cat> {
+    return this.catModel
+      .findByIdAndUpdate(id, updateCatDto, { new: true })
+      .exec();
+  }
+
+  async upsert(filter: object, updateCatDto: CreateCatDto): Promise<Cat> {
+    return this.catModel
+      .findOneAndUpdate(filter, updateCatDto, { new: true, upsert: true })
+      .exec();
   }
 
   async delete(id: string) {
