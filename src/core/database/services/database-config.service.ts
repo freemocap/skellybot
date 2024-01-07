ea5235a45bbc8f8197875184c25fa049fa65b5fe
jonmatthis <jonmatthis@gmail.com>
2024-01-06 19:37:19 -0500

    Setting up dev vs prod env stuff for MongoDB

--- src/core/database/services/database-config.service.ts
+++ src/core/database/services/database-config.service.ts
@@ -1,41 +1,32 @@
 import { Injectable } from '@nestjs/common';
-import { NecordModuleOptions } from 'necord';
-import { IntentsBitField } from 'discord.js';
 import { ConfigService } from '@nestjs/config';
 import { GcpSecretsService } from '../../gcp/gcp-secrets.service';
+import { MongooseModuleOptions } from '@nestjs/mongoose';
 
 @Injectable()
-export class DiscordConfigService {
+export class DatabaseConfigService {
   private _tokenMap = {
-    DISCORD_BOT_TOKEN:
-      'projects/588063171007/secrets/DISCORD_BOT_TOKEN/versions/latest',
+    MONGODB_URI: 'projects/588063171007/secrets/MONGODB_URI/versions/latest',
   };
   constructor(
     private readonly sms: GcpSecretsService,
     private readonly _cfgService: ConfigService,
   ) {}
 
-  async createNecordOptions(): Promise<NecordModuleOptions> {
-    const token = await this._createTokenByNodeEnv();
-    return {
-      token,
-      intents: [
-        IntentsBitField.Flags.Guilds,
-        IntentsBitField.Flags.GuildMessages,
-        IntentsBitField.Flags.MessageContent,
-      ],
-    };
+  async createMongooseOptions(): Promise<MongooseModuleOptions> {
+    const uri = await this._getUriByNodeEnv();
+    return { uri };
   }
 
-  private async _createTokenByNodeEnv() {
-    if (process.env.NODE_ENV === 'production') {
-      const secretName = this._tokenMap.DISCORD_BOT_TOKEN;
+  private async _getUriByNodeEnv() {
+    if (process.env.NODE_ENV === 'production' || true) {
+      const secretName = this._tokenMap.MONGODB_URI;
       const [secret] = await this.sms.getSecretsManager().accessSecretVersion({
         name: secretName,
       });
       return secret.payload.data.toString();
     }
 
-    return this._cfgService.getOrThrow('DISCORD_BOT_TOKEN');
+    return this._cfgService.getOrThrow('MONGODB_URI');
   }
 }
