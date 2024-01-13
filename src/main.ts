042e1b9eee40ab55edabb9a9e4e8f6d36b3f6da0
jonmatthis <jonmatthis@gmail.com>
2024-01-13 08:47:39 -0500

    push to trigger GH Action

--- src/main.ts
+++ src/main.ts
@@ -20,6 +20,6 @@ async function bootstrap() {
   SwaggerModule.setup('api', app, document);
 
   await app.listen(3000);
-  console.log(`Application is running on: ${await app.getUrl()}`);
+  console.log(`SkellyBot Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
