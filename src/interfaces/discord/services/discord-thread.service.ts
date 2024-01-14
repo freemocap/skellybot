719e7c44925408db3390865e17a90331bef08183
jonmatthis <jonmatthis@gmail.com>
2024-01-14 06:43:06 -0500

    dummy `get context instructions` method

--- src/interfaces/discord/services/discord-thread.service.ts
+++ src/interfaces/discord/services/discord-thread.service.ts
@@ -81,8 +81,8 @@ export class DiscordThreadService implements OnModuleDestroy {
         name: thread.name,
       },
     );
+    const contextInstructions = this._getContextInstructions(channel);
 
-    const contextInstructions = channel.topic || '';
     this._logger.log(
       `Creating bot with contextInstructions: \n ''' \n ${contextInstructions}\n '''`,
     );
@@ -102,6 +102,18 @@ export class DiscordThreadService implements OnModuleDestroy {
     });
     this._beginWatchingIncomingMessages(interaction, channel, thread);
     await this._sendInitialReply(interaction, channel, thread, text);
+  }
+
+  private _getContextInstructions(channel: TextChannel) {
+    const channelInstructions = channel.topic || '';
+    const categoryInstructions = ''; // TODO: get category instructions from category-level `bot-config`  channel
+    const serverInstructions = ''; // TODO: get server instructions from server-level `bot-config` channel
+    const contextInstructions = [
+      serverInstructions,
+      categoryInstructions,
+      channelInstructions,
+    ].join('\n');
+    return contextInstructions;
   }
 
   /**
