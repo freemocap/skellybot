9acfa3b4a06f6c7e9a191816e5b374cd1576279e
John K. Lindstedt <john.k.lindstedt@gmail.com>
2025-01-29 15:02:39 -0500

    /fetch_youtube_transcript fully functional

--- src/interfaces/discord/commands/youtube-transcript-request.dto.ts
+++ src/interfaces/discord/commands/youtube-transcript-request.dto.ts
@@ -4,7 +4,7 @@ export class YoutubeTranscriptRequestDto {
   @StringOption({
     name: 'video_id',
     description:
-      'The youtube video id (the part after "youtube.com/watch?v=", e.g. "dQw4w9WgXcQ")',
+      'The youtube video id (e.g., "youtube.com/watch?v=[video_id]" or "youtu.be/[video_id]/")',
     required: true,
   })
   video_id: string = '';
