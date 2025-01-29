dfc6ce866729236d0f30946e49e2b97053d5b6f6
John K. Lindstedt <john.k.lindstedt@gmail.com>
2025-01-29 15:03:10 -0500

    string tweak

--- src/interfaces/discord/commands/youtube-transcript-request.dto.ts
+++ src/interfaces/discord/commands/youtube-transcript-request.dto.ts
@@ -4,7 +4,7 @@ export class YoutubeTranscriptRequestDto {
   @StringOption({
     name: 'video_id',
     description:
-      'The youtube video id (e.g., "youtube.com/watch?v=[video_id]" or "youtu.be/[video_id]/")',
+      'The youtube video id (e.g., "youtube.com/watch?v=[video_id]&..." or "youtu.be/[video_id]/")',
     required: true,
   })
   video_id: string = '';
